ca7f78057f28ea0c8a642f45554506a634318b3d
Paolo Rechia <paolorechia@gmail.com>
2021-01-03 11:27:03 -0300

    Dashboard UI

--- Contracoes WatchKit Extension/Views/Dashboard.swift
+++ Contracoes WatchKit Extension/Views/Dashboard.swift
@@ -11,28 +11,32 @@ struct Dashboard: View {
     var body: some View {
         VStack {
             HStack {
-                Text("Count (last hour):")
+                Text("Recent Contractions")
+                    .font(.footnote)
                 Text("3")
             }
             .padding()
             HStack {
-                Text("Average Duration:")
+                Text("Average Duration")
+                    .font(.footnote)
+
                 Text("34")
             }
             .padding()
             HStack {
-                Text("")
+                Text("Average Interval")
+                    .font(.footnote)
                 Text("123")
             }
             .padding()
         }
-        .background(Color.blue)
-
+        .background(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.10))
     }
 }
 
 struct Dashboard_Previews: PreviewProvider {
     static var previews: some View {
         Dashboard()
+            .previewDevice("Apple Watch Series 5 - 40mm")
     }
 }
